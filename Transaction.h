10306eff00c6d65cc51040b948c920606d149759
Tiziano <t.talini@libero.it>
2025-02-07 18:03:05 +0100

    primo commit

---
+++ Transaction.h
@@ -0,0 +1,16 @@
+//
+// Created by tizi2808 on 07/02/25.
+//
+
+#ifndef TRANSACTION_H
+#define TRANSACTION_H
+#include <string>
+
+#endif //TRANSACTION_H
+using namespace std;
+class Transaction {
+private:
+    string data;
+    double amount;
+};
+
