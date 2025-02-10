6f4876eb56dc675cd0c9a7153e174ed5ee57d918
Tiziano <t.talini@libero.it>
2025-02-10 15:45:25 +0100

    project updated

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
 
-int main() {
+#include "BankAccount.h"
 
+int BankAccount::counter = 0;
+int Transaction::counter = 0;
+
+int main() {
+    BankAccount* acc1 = new BankAccount();
+    BankAccount* acc2 = new BankAccount();
+    acc1->deposit(100);
+    acc2->deposit(100);
+    BankAccount& account = *acc1;
+    account.deposit(100);
+    account.displayBalance();
+    account = *acc2;
+    account.displayBalance();
+    account.withdraw(100);
+    account.displayBalance();
+    account.displayTransactions();
 
     return 0;
 }
