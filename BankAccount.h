3ad67cf43a59526b0a3a6ceed9618584cee42f61
tizi2808 <t.talini@libero.it>
2025-02-14 18:29:23 +0100

    db creation and transaction saving

--- BankAccount.h
+++ BankAccount.h
@@ -7,17 +7,20 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <filesystem>
 #include "Deposit.h"
 #include "Withdrawal.h"
 #include "BankTransfer.h"
 
-using namespace std;
+namespace fs = std::filesystem;
 
 class BankAccount {
     static int counter;
-    string id;
+    std::string id;
     double balance = 0;
-    vector<Transaction*> transactions;
+    std::vector<Transaction*> transactions;
+    static std::string db;
 
     void increaseBalance(const double amount) {
         balance += amount;
@@ -27,7 +30,10 @@ class BankAccount {
     }
 public:
     BankAccount() {
-        id = to_string(counter++);
+        id = std::to_string(counter++);
+    }
+    explicit BankAccount(const std::string& id) {
+        this->id = id;
     }
     ~BankAccount() {
         for (const auto t : transactions) {
@@ -40,13 +46,11 @@ public:
     void deposit(const double amount) {
         auto transaction = new Deposit(*this, amount);
         transaction->execute();
-        transactions.push_back(transaction);
     }
 
     void withdraw(const double amount) {
         auto transaction = new Withdrawal(*this, amount);
         transaction->execute();
-        transactions.push_back(transaction);
     }
     void applyTransaction(Transaction& transaction) {
         const double amount = transaction.getAmount();
@@ -61,13 +65,99 @@ public:
                 this->increaseBalance(amount);
             }
         }
+        transactions.push_back(&transaction);
+        storeTransaction(transaction);
+    }
+
+    static void createDB() {
+        fs::path dir{db};
+
+        // Crea la directory se non esiste
+        if (!fs::exists(dir)) {
+            if (fs::create_directory(dir))
+                std::cout << "Directory 'db' creata con successo" << std::endl;
+            else {
+                std::cerr << "Errore nella creazione della directory 'db'" << std::endl;
+            }
+        }
+    }
+/*
+    void readStateFromFile(std::string& filename) {
+        std::ifstream fileIn(db + "/" + filename);
+        std::string line;
+        std::string savedData;
+        std::string balanceLine;
+        if (fileIn.is_open()) {
+            if (std::getline(fileIn, line)) {
+                balanceLine = line;
+            }
+            while (std::getline(fileIn, line)) {
+                savedData += line + "\n";
+            }
+            fileIn.close();
+        }
+        size_t posB = balanceLine.find('B');
+
+        std::string id = balanceLine.substr(2, posB-2);
+        if (posB == std::string::npos) {
+            std::cerr << "Formato errato: manca 'B'" << std::endl;
+        }
+        this->id = id;
+        double balance = std::stod(balanceLine.substr(posB+1));
+        this->balance = balance;
+        std::istringstream previousTransactions(savedData);
+        while (std::getline(previousTransactions, line)) {
+
+        }
+
+    }
+*/
+    void storeTransaction(Transaction& transaction) {
+        std::string filename = id + ".txt";
+        try {
+            std::ifstream fileIn(db + "/" + filename);
+            std::string out;
+            std::ostringstream savedData;
+
+            if (fileIn.is_open()) {
+                std::string line;
+                if (std::getline(fileIn, line)) {
+                    std::string balanceLine;
+                    balanceLine = line;
+                }
+                while (std::getline(fileIn, line)) {
+                    savedData << line << std::endl;
+                }
+                fileIn.close();
+            }
+
+            /*
+            if (balanceLine.empty() || balanceLine[0] != 'B') {
+                std::cerr << "Il file è vuoto o il formato non è valido." << std::endl;
+            }
+            */
+
+            out += "ID" + id + "B" + std::to_string(balance) + "\n";
+            out += savedData.str() + "\n";
+            out += transaction.serialize() + "\n";
+
+            std::ofstream fileOut(db + "/" + filename);
+            fileOut << out;
+            fileOut.close();
+        }catch (const std::exception& err) {
+            std::cout << err.what() << std::endl;
+        }
+    }
+
+    void saveState() {
+
     }
 
     void displayBalance() const {
-        cout << "Saldo: " << balance << endl;
+        std::cout << "Saldo conto " << id << ": " << balance << std::endl;
     }
     void displayTransactions() const {
-        cout << "Transactions for " << id << ": " << endl;
+        std::cout << "Transactions for " << id << ": " << std::endl;
         for (const auto t : transactions) {
             t->display();
         }
