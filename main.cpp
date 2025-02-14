02299939678ce711e0ad1b4e8da192f436cd7845
tizi2808 <t.talini@libero.it>
2025-02-14 18:30:17 +0100

    project updated

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 
 int BankAccount::counter = 0;
 int Transaction::counter = 0;
+std::string BankAccount::db = "db";
 
 int main() {
     BankAccount* acc1 = new BankAccount();
@@ -13,11 +14,11 @@ int main() {
     BankAccount& account = *acc1;
     account.deposit(100);
     account.displayBalance();
-    account = *acc2;
-    account.displayBalance();
-    account.withdraw(100);
-    account.displayBalance();
-    account.displayTransactions();
+    BankAccount& account2 = *acc2;
+    account2.displayBalance();
+    account2.withdraw(100);
+    account2.displayBalance();
+    account2.displayTransactions();
 
     return 0;
 }
